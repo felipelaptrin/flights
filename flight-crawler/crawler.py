d01d80b499250a7c99cca5828433e03ea47f5d81
Felipe Trindade <felipelaptrin@gmail.com>
2023-03-04 20:34:25 -0300

    fix version of dependencies

--- flight-crawler/crawler.py
+++ flight-crawler/crawler.py
@@ -38,7 +38,7 @@ class Crawler:
             self.logger.debug("Selenium Driver set correctly to work locally with headers")
             return driver
         options = uc.ChromeOptions()
-        options.binary_location = self.CHROME_BINARY_PATH
+        # options.binary_location = self.CHROME_BINARY_PATH
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
@@ -52,7 +52,7 @@ class Crawler:
         options.add_argument(f"--disk-cache-dir={mkdtemp()}")
         options.add_argument("--remote-debugging-port=9222")
 
-        driver = uc.Chrome(self.CHROME_WEBDRIVER_BINARY_PATH, options=options)
+        driver = uc.Chrome(options=options)
         self.logger.debug("Selenium Driver set correctly")
         return driver
 
