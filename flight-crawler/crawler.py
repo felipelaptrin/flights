033370fdb7259865c1980c984c51d32d4a32a9d1
Felipe Trindade <felipelaptrin@gmail.com>
2023-03-04 22:41:53 -0300

    fix ci

--- flight-crawler/crawler.py
+++ flight-crawler/crawler.py
@@ -38,6 +38,7 @@ class Crawler:
             self.logger.debug("Selenium Driver set correctly to work locally with headers")
             return driver
         options = uc.ChromeOptions()
+        options.binary_location = self.CHROME_BINARY_PATH
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
@@ -51,7 +52,7 @@ class Crawler:
         options.add_argument(f"--disk-cache-dir={mkdtemp()}")
         options.add_argument("--remote-debugging-port=9222")
 
-        driver = uc.Chrome(options=options)
+        driver = uc.Chrome(self.CHROME_WEBDRIVER_BINARY_PATH, options=options)
         self.logger.debug("Selenium Driver set correctly")
         return driver
 
