970cebac45e71ff82e9117d16b6772b618cf5d6c
Felipe Trindade <felipelaptrin@gmail.com>
2023-03-04 22:47:41 -0300

    wip

--- flight-crawler/crawler.py
+++ flight-crawler/crawler.py
@@ -38,19 +38,8 @@ class Crawler:
             self.logger.debug("Selenium Driver set correctly to work locally with headers")
             return driver
         options = uc.ChromeOptions()
+        options.headless = True
         options.binary_location = self.CHROME_BINARY_PATH
-        options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--window-size=1280x1696")
-        options.add_argument("--single-process")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-dev-tools")
-        options.add_argument("--no-zygote")
-        options.add_argument(f"--user-data-dir={mkdtemp()}")
-        options.add_argument(f"--data-path={mkdtemp()}")
-        options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-        options.add_argument("--remote-debugging-port=9222")
 
         driver = uc.Chrome(self.CHROME_WEBDRIVER_BINARY_PATH, options=options)
         self.logger.debug("Selenium Driver set correctly")
